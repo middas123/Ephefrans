1ee70682781f77a5af326a7f9d488b8e29ec60d3
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2024-03-05 12:19:09 +0200

    added content on cntact page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 offset-lg-3">
+            <h2 className="section-title">Contact Us</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" className="form-control" id="name" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" className="form-control" id="email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="subject">Subject</label>
+                <input type="text" className="form-control" id="subject" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea className="form-control" id="message" rows="5"></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
